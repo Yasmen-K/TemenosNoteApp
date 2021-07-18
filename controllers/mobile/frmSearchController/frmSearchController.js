ee36877b4d54127a59b95ca69cf93c25b07cab23
Yasmen-K <kodjabashevayasemin@gmail.com>
2021-07-18 21:33:59 +0300

    listBox modified

--- controllers/mobile/frmSearchController/frmSearchController.js
+++ controllers/mobile/frmSearchController/frmSearchController.js
@@ -1,5 +1,27 @@
 define({ 
 
  //Type your controller code here 
+  
+    onViewCreated: function() {
+    this.view.init = this.init;
+    this.view.preShow = this.preShow;
+   
+  },
+  
+    init: function() {
+  
+  },
+  
+  preShow: function() {
+    this.view.listBoxCategory.masterData = this.dataList;
+  },
+  
+  
+  dataList :[
+    ["key1", "burger"],
+    ["key2", "spice"],
+    ["key3", "car"]
+],
+
 
  });
