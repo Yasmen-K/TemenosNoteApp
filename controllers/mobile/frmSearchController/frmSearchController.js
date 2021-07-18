fbf1d0b4c0af9c3774fa2fd9917ceb3b747dc004
Yasmen-K <kodjabashevayasemin@gmail.com>
2021-07-18 22:36:53 +0300

    FrmSearch function for selected value

--- controllers/mobile/frmSearchController/frmSearchController.js
+++ controllers/mobile/frmSearchController/frmSearchController.js
@@ -14,14 +14,32 @@ define({
   
   preShow: function() {
     this.view.listBoxCategory.masterData = this.dataList;
+    this.view.listBoxCategory.onSelection =this.onSelectionCallback;
+   
   },
   
+  
+
   
   dataList :[
     ["key1", "burger"],
     ["key2", "spice"],
     ["key3", "car"]
 ],
+  
+
+  
+  onSelectionCallback:function(){
+  var selected = this.view.listBoxCategory.selectedKey;
+  var found = this.dataList.find(function(el){
+    //send found[1]
+    if(el[0] === selected){
+      
+      return el;
+    }
+  });   
+
+  },
 
 
  });
