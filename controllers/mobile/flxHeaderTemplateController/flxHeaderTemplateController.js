9b1761f3b603017c1d185b8c496b40dfa1f24921
StoyanKostadinov97 <stoyanKostadinov@outlook.com>
2021-07-20 14:04:18 +0300

    added skins and data

--- controllers/mobile/flxHeaderTemplateController/flxHeaderTemplateController.js
+++ controllers/mobile/flxHeaderTemplateController/flxHeaderTemplateController.js
@@ -1,10 +1,5 @@
 define({ 
 	
-//   onViewCreated:function(){
-//     this.view.imgAddCategory.onClick=function(){
-//       console.log(this);
-//     };
-//   }
  //Type your controller code here 
 
  });
--- controllers/mobile/frmCategoriesListController/frmCategoriesListController.js
+++ controllers/mobile/frmCategoriesListController/frmCategoriesListController.js
@@ -1,157 +1,156 @@
-define({ 
-  categories: [
-    {name: "Movie",
-     data:[
-       {title: "Ivan Minchev",
-        edited: "December 29 2009",
-        marker: "25/06/2021"},
-       {name: "Denica Stoeva",
-        edited: "August 25 2010",
-        marker: "25/06/2021"},
-       {title: "Magazin za mebeli",
-        edited: "Mar 12 2012",
-        marker: "25/06/2021"},
-       {title: "Ivan Minchev",
-        edited: "June 14 2020",
-        marker: "25/06/2021"},
-     ]},
-    {name: "Category",
-     data:[
-       {title: "Ivan Minchev",
-        edited: "December 29 2009",
-        marker: "25/06/2021"},
-       {name: "Denica Stoeva",
-        edited: "August 25 2010",
-        marker: "25/06/2021"},
-       {title: "Magazin za mebeli",
-        edited: "Mar 12 2012",
-        marker: "25/06/2021"},
-       {title: "Ivan Minchev",
-        edited: "June 14 2020",
-        marker: "25/06/2021"},
-     ]},
-    {name: "Notes",
-     data:[
-       {title: "Ivan Minchev",
-        edited: "December 29 2009",
-        marker: "25/06/2021"},
-       {name: "Denica Stoeva",
-        edited: "August 25 2010",
-        marker: "25/06/2021"},
-       {title: "Magazin za mebeli",
-        edited: "Mar 12 2012",
-        marker: "25/06/2021"},
-       {title: "Ivan Minchev",
-        edited: "June 14 2020",
-        marker: "25/06/2021"},
-     ]},
-    {name: "Category 4",
-     data:[
-       {title: "Ivan Minchev",
-        edited: "December 29 2009",
-        marker: "25/06/2021"},
-       {name: "Denica Stoeva",
-        edited: "August 25 2010",
-        marker: "25/06/2021"},
-       {title: "Magazin za mebeli",
-        edited: "Mar 12 2012",
-        marker: "25/06/2021"},
-       {title: "Ivan Minchev",
-        edited: "June 14 2020",
-        marker: "25/06/2021"},
-     ]}
-  ],
+  define({ 
+    categories: [
+      {name: "Kony Widgets",
+       data:[
+         {title: "Button",
+          description:"Button widget is a UI element that is used to trigger any actions. These actions can be navigating to another form, perform animations, interacting with a dialog box, etc.",
+          edited: "December 29 2009",
+          categories:["Widgets"],
+          marker: "sknCircleRed"},
+         {title: "Flex Container",
+          description:"The FlexContainer widget is a container that can provide a unique UI/UX property to a collection of widgets.",
+          edited: "August 25 2010",
+          categories:["Widgets"],
+          marker: "sknCircleBlue"},
+         {title: "Image",
+          description:"Image widget is used to display a graphic image such as a company logo, photo, or illustration, in PNG, JPEG,WebP or GIF format, in an application.",
+          edited: "Mar 12 2012",
+          categories:["Widgets"],
+          marker: "sknCircleBlue"},
+         {title: "List Box",
+          description:"The ListBox widget displays a list of items as a drop-down box and allows you to select a one or more items from the list.",
+          edited: "June 14 2020",
+          categories:["Widgets"],
+          marker: "sknCircleRed"},
+         {title: "Label",
+          description:"The Label widget is used to display static text on a form. It can be used as a form label or an area header with various text alignments and styles.",
+          edited: "August 25 2010",
+          categories:["Widgets"],
+          marker: "sknCircleOrange"},
+         {title: "Map",
+          description:"aIf you want to display certain geographical locations on a map as part of your application, you should use a Map widget.",
+          edited: "Mar 12 2012",
+          categories:["Widgets"],
+          marker: "sknCircleGreen"},
+         {title: "Text Box",
+          description:"The TextBox widget enables you to get a single-line input from users. This widget can accept input from the users in the form of numbers or text.",
+          edited: "June 14 2020",
+          categories:["Widgets"],
+          marker: "sknCircleYellow"}
+       ]},
+      {name: "Kony Fabric",
+       data:[
+         {title: "Identity",
+          description:"Quantum Fabric identity services help you secure your application by adding an authentication layer.",
+          edited: "December 29 2009",
+          categories:["Kony Fabric"],
+          marker: "sknCircleYellow"},
+         {name: "Integration",
+          description:"An Integration Service is an application component that represents the application interaction with an external system or data source.",
+          edited: "August 25 2010",
+          categories:["Kony Fabric"],
+          marker: "sknCircleOrange"},
+         {title: "Orchestration",
+          description:"Orchestration Services leverage the concept of combining multiple integration services, object services or orchestration services into a single service to simplify business logic in client apps and reduce the number of service invocations.",
+          edited: "Mar 12 2012",
+          categories:["Kony Fabric"],
+          marker: "sknCircleBlue"},
+         {title: "Object Services",
+          description:"Object Services is a feature of Quantum Fabric that enables model-driven application design and development by following a microservices architectural approach to create reusable components and link them to fit into your solution.",
+          edited: "June 14 2020",
+          categories:["Kony Fabric"],
+          marker: "sknCircleRed"},
+       ]}
+    ],
 
-  formatedData: [],
+    formatedData: [],
 
-  onViewCreated: function(){
-    this.view.init = this.init;
-    this.view.preShow = this.preShow;
-    this.view.postShow = this.postShow;
-  },
+    onViewCreated: function(){
+      this.view.init = this.init;
+      this.view.preShow = this.preShow;
+    },
 
-  init: function() {
-    debugger;
-    this.renderSegList();
-  },
+    init: function() {
+      this.renderSegList();
+    },
 
-  preShow: function() {
-    debugger;
-    this.view.segListCategories.onRowClick = this.onRowClicked;
-    this.view.segListCategories.setData(this.formatedData);
-    this.view.btnSaveCategory.onClick=this.saveCategory;
-    this.view.btnCloseIcon.onClick=this.closePopup;
-  },
+    preShow: function() {
+      this.view.reusableHeader.RoundFloatingButton.onClick=this.navToSearch;
+      this.view.segListCategories.onRowClick = this.onRowClicked;
+      this.view.segListCategories.setData(this.formatedData);
+      this.view.btnSaveCategory.onClick=this.saveCategory;
+      this.view.btnCloseIcon.onClick=this.closePopup;
+    },
 
-  postShow: function(){  
-    debugger;
-    this.view.segListCategories.data[0][0].btnAddCategory.onClick=this.addCategory;
-    debugger;
-  },
+    navToSearch:function(){
+      kony.store.setItem("categories", this.categories);
+      var konyNavigate = new kony.mvc.Navigation("frmSearch");
+      konyNavigate.navigate();
+    },
 
-  formatedSegmentData: function(responseData,formatedData) {
-    var self=this;
-    formatedData=[];
+    formatedSegmentData: function(responseData,formatedData) {
+      var self=this;
+      formatedData=[];
 
-    var header=[];
-    debugger;
-    header.push({
-      "lblHeaderTitle":{"text":"Categories"},
-      "btnAddCategory":{"skin":"sknBtnAddImg","onClick":self.addCategory}
-    });
-    //     debugger;
-
-    responseData.sort(this.compare);
-
-    var rows=[];
-    responseData.forEach(function(category) {
-      rows.push({
-        "lblCategoryName": {"text": category.name},
-        "lblNotesNumber": {"text": category.data.length},
-        //         "imgArrow": {"text": contact.dateAdded},
-        "lblBottomLine": {"width": "95%"}
+      var header=[];
+      header.push({
+        "lblHeaderTitle":{"text":"Categories"},
+        "btnAddCategory":{"skin":"sknBtnAddImg","onClick":self.addCategory}
       });
-    });
 
-    header.push(rows);
-    formatedData.push(header);
-    this.formatedData=formatedData;
-  },
+      responseData.sort(this.compare);
 
-  renderSegList:function(){
-    this.formatedSegmentData(this.categories, this.formatedData);
-    this.view.segListCategories.setData(this.formatedData);
-  },
+      var rows=[];
+      responseData.forEach(function(category) {
+        rows.push({
+          "lblCategoryName": {"text": category.name},
+          "lblNotesNumber": {"text": Math.round(category.data.length)},
+          //         "imgArrow": {"text": contact.dateAdded},
+          "flxBottomLine": {"width": "95%"}
+        });
+      });
 
-  compare:function(a,b){
-    var first=a.name;
-    var second=b.name;
-    return ( first < second ? -1 : ( first > second ? 1 : 0 ));
-  },
+      header.push(rows);
+      formatedData.push(header);
+      this.formatedData=formatedData;
+    },
 
-  onRowClicked: function() {
-    var indexOfSelectedRow = this.view.segListCategories.selectedRowIndex[1];
-    var data = this.categories[indexOfSelectedRow];
-    kony.store.setItem("choosenCategory", data);
-    //TODO navigation
-  },
+    renderSegList:function(){
+      this.formatedSegmentData(this.categories, this.formatedData);
+      this.view.segListCategories.setData(this.formatedData);
+    },
 
-  addCategory: function(){
-    this.view.flxPopup.setVisibility(true);
-    this.view.flxPopup.isModalContainer=true;
-  },
+    compare:function(a,b){
+      var first=a.name;
+      var second=b.name;
+      return ( first < second ? -1 : ( first > second ? 1 : 0 ));
+    },
 
-  saveCategory:function(){
-    var title=this.view.txtBoxAddCategory.text;
-    this.categories.push({
-      name:title,
-      data:[]
-    });
-    this.renderSegList();    
-    this.closePopup();
-  },
+    onRowClicked: function() {
+      var indexOfSelectedRow = this.view.segListCategories.selectedRowIndex[1];
+      var data = this.categories[indexOfSelectedRow];
+      kony.store.setItem("categories", this.categories);
+      kony.store.setItem("currentCategory", data);
+      var konyNavigate = new kony.mvc.Navigation("frmCategoryNotes");
+      konyNavigate.navigate();
+    },
 
-  closePopup:function(){
-    this.view.flxPopup.setVisibility(false);
-  }
-});
+    addCategory: function(){
+      this.view.flxPopup.setVisibility(true);
+      this.view.flxPopup.isModalContainer=true;
+    },
+
+    saveCategory:function(){
+      var title=this.view.txtBoxAddCategory.text;
+      this.categories.push({
+        name:title,
+        data:[]
+      });
+      this.renderSegList();    
+      this.closePopup();
+    },
+
+    closePopup:function(){
+      this.view.flxPopup.setVisibility(false);
+    }
+  });
