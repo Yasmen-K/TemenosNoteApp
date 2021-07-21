5be1addceeedf2cce31011d14ea78972498d77de
Vladko5 <vladi_dimitrovv@abv.bg>
2021-07-21 16:30:16 +0300

    Update frmCategoryNotesController.js

--- controllers/mobile/frmCategoryNotesController/frmCategoryNotesController.js
+++ controllers/mobile/frmCategoryNotesController/frmCategoryNotesController.js
@@ -3,21 +3,14 @@ define({
   formatedNotes: [],
 
   onViewCreated: function() {
-    this.view.init = this.init;
     this.view.preShow = this.preShow;
-
   },
-
-
-  init: function() {
-    this.sortNotes = this.sortNotes;
+  
+  preShow: function() {
+    this.formatedNotes = [];
     this.konyIndex = kony.store.getItem("categoryIndex");
     this.konyCategories = kony.store.getItem("categories");
     this.formatNotesData(this.konyIndex,this.konyCategories,this.formatedNotes);
-  },
-
-  preShow: function() {
-
     this.view.segNotes.setData(this.formatedNotes);
     this.view.segNotes.onRowClick = this.onRowClicked;
     this.view.reusableHeader.btnSearch.onClick = this.navigate;
